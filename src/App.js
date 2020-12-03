5200f21e4ae6413400bd4edd253e3f4cf93d743e
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-12-03 15:46:33 -0800

    added a header and some semantic-ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { Component } from "react";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Header />
+        <Wrapper></Wrapper>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+
+const Header = () => {
+  const [activeItem, setActiveItem] = useState();
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
+  return (
+    <div id="header">
+      <Menu pointing secondary>
+        <Menu.Item
+          as={NavLink}
+          to="/"
+          name="home"
+          id="home-nav"
+          onClick={handleItemClick}
+        >
+          Home
+        </Menu.Item>
+        <Menu.Item
+          as={NavLink}
+          to="/portfolio"
+          name="portfolio"
+          active={activeItem === "portfolio"}
+          onClick={handleItemClick}
+        >
+          Portfolio
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
+
+export default Header;
